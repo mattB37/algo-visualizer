e2f81424391bc8646da9ac72f0eed47a307debbe
Matthew Bosch <43504472+mattB37@users.noreply.github.com>
2023-07-06 20:49:21 -0500

    changed css of sort visualizer page

--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -17,6 +17,7 @@ const SortingVisualizer: React.FC = () => {
     <div>
       <BlockArray blocks={blocks} />
       <select
+        className="text-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         disabled={isSorting}
         value={algorithm}
         onChange={(e) => setAlgorithm(e.target.value as AlgorithmName)}
@@ -28,6 +29,7 @@ const SortingVisualizer: React.FC = () => {
         ))}
       </select>
       <select
+        className="text-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         disabled={isSorting}
         value={speed}
         onChange={(e) => setSpeed(Number(e.target.value))}
@@ -40,10 +42,18 @@ const SortingVisualizer: React.FC = () => {
         <option value={100}>100x</option>
         <option value={1000}>1000x</option>
       </select>
-      <button onClick={generateSteps} disabled={isSorting}>
+      <button
+        className="text-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        onClick={generateSteps}
+        disabled={isSorting}
+      >
         Sort
       </button>
-      <button onClick={randomizeBlocks} disabled={isSorting}>
+      <button
+        className="text-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        onClick={randomizeBlocks}
+        disabled={isSorting}
+      >
         Randomize Array
       </button>
     </div>
