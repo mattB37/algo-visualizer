b4f088f407eb958588084dac3d70a8dfb38418a8
Matthew Bosch <43504472+mattB37@users.noreply.github.com>
2023-07-23 11:03:24 -0500

    added clear visualization button, implemented dfs, updated dfs and bfs to run from each unvisited node in the graph (for graphs with multiple seperate components)

--- src/components/GraphVisualizer.tsx
+++ src/components/GraphVisualizer.tsx
@@ -106,16 +106,21 @@ const GraphVisualizer: React.FC = () => {
   };
 
   const clearAllNodes = () => {
-    setNodes([
-      {
-        id: "1",
-        type: "nodeVis",
-        data: { value: 1, startNode: true, visited: false, visiting: false },
-        position: { x: 5, y: 5 },
-        className: "circle",
+    setNodes(initialNodes);
+    setEdges(initialEdges);
+  };
+
+  const resetVisualization = () => {
+    const updatedNodes = nodes.map((node) => ({
+      ...node,
+      data: {
+        ...node.data,
+        visited: false,
+        visiting: false,
       },
-    ]);
-    setEdges([]);
+    }));
+
+    setNodes(updatedNodes);
   };
 
   return (
@@ -183,6 +188,14 @@ const GraphVisualizer: React.FC = () => {
         disabled={isSearching}
       >
         Clear All Nodes
+      </button>
+
+      <button
+        onClick={resetVisualization}
+        className="text-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        disabled={isSearching}
+      >
+        Reset visualization
       </button>
       <button
         className="text-black bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
--- src/utils/graphAlgorithms.ts
+++ src/utils/graphAlgorithms.ts
@@ -32,54 +32,115 @@ const BFSSteps = (
   const q: string[] = [];
   const visited = new Set<string>();
   const newSearchSteps: GraphStorage[] = [];
-  q.push(startNode); //starting node is always 1
+
+  const bfsIterative = () => {
+    while (q.length > 0) {
+      for (let i = 0; i < q.length; i++) {
+        let n = q.shift();
+        if (!n) {
+          return newSearchSteps;
+        }
+        visited.add(n);
+        nodes[parseInt(n, 10) - 1].data.visited = true;
+        newSearchSteps.push({
+          nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
+          edges: edges.map((edge) => ({ ...edge })),
+        });
+
+        for (const neighbor of adj.get(n)!) {
+          if (visited.has(neighbor.toString())) {
+            continue;
+          }
+          nodes[neighbor - 1].data.visiting = true;
+          q.push(neighbor.toString());
+        }
+        newSearchSteps.push({
+          nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
+          edges: edges.map((edge) => ({ ...edge })),
+        });
+        for (const neighbor of adj.get(n)!) {
+          if (visited.has(neighbor.toString())) {
+            continue;
+          }
+          nodes[neighbor - 1].data.visiting = false;
+        }
+      }
+    }
+  };
+
+  q.push(startNode);
   visited.add(startNode);
-  console.log(newSearchSteps);
-  console.log("hi");
   newSearchSteps.push({
     nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
     edges: edges.map((edge) => ({ ...edge })),
   });
+  bfsIterative();
 
-  console.log("hi");
-  console.log(newSearchSteps);
-  while (q.length > 0) {
-    for (let i = 0; i < q.length; i++) {
-      let n = q.shift();
-      if (!n) {
-        return newSearchSteps;
-      }
-      visited.add(n);
-      nodes[parseInt(n, 10) - 1].data.visited = true;
-      newSearchSteps.push({
-        nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
-        edges: edges.map((edge) => ({ ...edge })),
-      });
-
-      for (const neighbor of adj.get(n)!) {
-        if (visited.has(neighbor.toString())) {
-          continue;
-        }
-        nodes[neighbor - 1].data.visiting = true;
-        q.push(neighbor.toString());
-      }
-      newSearchSteps.push({
-        nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
-        edges: edges.map((edge) => ({ ...edge })),
-      });
-      for (const neighbor of adj.get(n)!) {
-        if (visited.has(neighbor.toString())) {
-          continue;
-        }
-        nodes[neighbor - 1].data.visiting = false;
-      }
+  for (const n of nodes) {
+    let v = n.data.value.toString();
+    if (visited.has(v)) {
+      continue;
     }
+    q.push(v);
+    visited.add(startNode);
+    newSearchSteps.push({
+      nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
+      edges: edges.map((edge) => ({ ...edge })),
+    });
+    bfsIterative();
   }
-  console.log(newSearchSteps);
+
+  return newSearchSteps;
+};
+
+const DFSSteps = (
+  nodes: Node<CustomNodeData>[],
+  edges: Edge[]
+): GraphStorage[] => {
+  const adj = createAdjList(nodes, edges);
+  const startNode = "1";
+  const visited = new Set<string>();
+  const newSearchSteps: GraphStorage[] = [];
+
+  const dfsRecursive = (n: string) => {
+    if (!n) {
+      return;
+    }
+    visited.add(n);
+    nodes[parseInt(n, 10) - 1].data.visiting = true;
+    newSearchSteps.push({
+      nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
+      edges: edges.map((edge) => ({ ...edge })),
+    });
+    for (const neighbor of adj.get(n)!) {
+      if (visited.has(neighbor.toString())) {
+        continue;
+      }
+      nodes[neighbor - 1].data.visiting = true;
+      dfsRecursive(neighbor.toString());
+    }
+    nodes[parseInt(n, 10) - 1].data.visiting = false;
+    nodes[parseInt(n, 10) - 1].data.visited = true;
+    newSearchSteps.push({
+      nodes: nodes.map((node) => ({ ...node, data: { ...node.data } })),
+      edges: edges.map((edge) => ({ ...edge })),
+    });
+  };
+
+  dfsRecursive(startNode);
+  for (const n of nodes) {
+    let v = n.data.value.toString();
+    if (visited.has(v)) {
+      continue;
+    }
+    dfsRecursive(v);
+  }
+
   return newSearchSteps;
 };
 
 // Export the sorting functions in an object, so they can be accessed by name
 export const graphAlgorithms = {
   "Breadth First Search": BFSSteps,
+  "Depth First Search": DFSSteps,
 };
